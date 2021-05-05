6c2d2b1fd17cabf00c654f7e17d2a50faf826803
Joubz <nathan.joubert.pro33@gmail.com>
2021-05-05 11:51:12 +0200

    feat: initialisation branche

---
+++ app/services/equipment.service.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { transaction } = require("../../database");
+
+/**
+ * Récupère un équipement depuis la bdd
+ * @param equipmentId
+ * @return {Promise<void>}
+ */
+const getEquipment = async (equipmentId) => {
+
+}
