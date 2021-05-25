1f5aa5827207acc84a7a667fac5fff50bf186442
Joubz <nathan.joubert.pro33@gmail.com>
2021-05-25 17:13:03 +0200

    fix: fix routes

--- app/routes/payment.routes.js
+++ app/routes/payment.routes.js
@@ -8,8 +8,6 @@ const router = express.Router();
 
 router.get("/:clientId", middlewares.auth.isAuthentified, controllers.payment.getCardPayment);
 
-router.get("/:clientId", middlewares.auth.isAuthentified, controllers.payment.getCardPayment);
-
 router.post("/add-card", middlewares.auth.isAuthentified, controllers.payment.postCardPayment);
 
 module.exports = router;
