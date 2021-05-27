ced0706f203659459ac0e5f97ff921d87417d283
BlackNat5937 <nathan.joubert.pro33@gmail.com>
2021-05-27 19:18:02 +0200

    fix: fix renvoie donne login renter

--- app/controllers/renter.controller.js
+++ app/controllers/renter.controller.js
@@ -12,23 +12,23 @@ const md5 = require('md5');
  * @returns L'erreur retournée par le service ou une confirmation (code 200)
  */
 const createRenter = async (req, res) => {
-	let newRenter = req.body.newRenter;
+    let newRenter = req.body.newRenter;
 
-	 newRenter.password = bcrypt.hashSync(newRenter.password, 8);
+    newRenter.password = bcrypt.hashSync(newRenter.password, 8);
 
-	try {
-		await services.renter.createRenter(newRenter);
+    try {
+        await services.renter.createRenter(newRenter);
 
-		await services.mail.sendConfirmationMail(
-			newRenter.firstName,
-			newRenter.lastName,
-			newRenter.email
-		);
-	} catch (err) {
-		return services.exception.generateException(err, res);
-	}
+        await services.mail.sendConfirmationMail(
+            newRenter.firstName,
+            newRenter.lastName,
+            newRenter.email
+        );
+    } catch (err) {
+        return services.exception.generateException(err, res);
+    }
 
-	res.status(200).json();
+    res.status(200).json();
 };
 
 /**
@@ -37,7 +37,7 @@ const createRenter = async (req, res) => {
  * @param {*} res Réponse Retourne la code http 200 et la liste des mails
  * @returns L'erreur retournée par le service
  */
- const getListMailRenter = async (req, res) => {
+const getListMailRenter = async (req, res) => {
     let listMailRenter = [];
 
     try {
@@ -49,46 +49,60 @@ const createRenter = async (req, res) => {
 };
 
 const loginRenter = async (req, res) => {
-	let foundUser = []; 
+    let foundUser = [];
 
-	let ipClient = req.headers["x-forwarded-for"] ? req.headers["x-forwarded-for"].split(',')[0] : req.ip
+    let ipClient = req.headers["x-forwarded-for"] ? req.headers["x-forwarded-for"].split(',')[0] : req.ip
 
-	try {
-		foundUser = await services.renter.loginRenter(req.body.email); 
-		
-		if (foundUser.length === 0) {
-			throw new services.exception.httpException('CLIENT_016'); 
-		}
-	} catch (err) {
-		return services.exception.generateException(err, res);
-	}
+    try {
+        foundUser = await services.renter.loginRenter(req.body.email);
 
-	 const isPasswordValid = bcrypt.compareSync (
-	 	req.body.password,
-	 	foundUser[0].PASSWORD
-	 );
+        if (foundUser.length === 0) {
+            throw new services.exception.httpException('CLIENT_016');
+        }
+    } catch (err) {
+        return services.exception.generateException(err, res);
+    }
 
-	if (!isPasswordValid) {
-		return services.exception.generateException(new services.exception.httpException('CLIENT_016'), res);
-	}
+    const isPasswordValid = bcrypt.compareSync (
+        req.body.password,
+        foundUser[0].PASSWORD
+    );
 
-	const token = jwt.sign(
-		{
-			id: foundUser[0].RENTER_ID,
-			email: foundUser[0].EMAIL, 
-			ipClient
-		},
-		"jwt-secret",
-		{
-			expiresIn: "7d"
-		}
-	);
+    if (!isPasswordValid) {
+        return services.exception.generateException(new services.exception.httpException('CLIENT_016'), res);
+    }
 
-	res.status(200).json({
-		id: foundUser[0].RENTER_ID, 
-		email: foundUser[0].EMAIL,
+    const token = jwt.sign(
+        {
+            id: foundUser[0].RENTER_ID,
+            email: foundUser[0].EMAIL,
+            ipClient
+        },
+        "jwt-secret",
+        {
+            expiresIn: "7d"
+        }
+    );
+
+    res.status(200).json({
+        renter : {
+            RENTER_ID: foundUser[0].RENTER_ID,
+            EMAIL: foundUser[0].EMAIL,
+            COMPANY_NAME: foundUser[0].COMPANY_NAME,
+            FIRST_NAME: foundUser[0].FIRST_NAME,
+            LAST_NAME: foundUser[0].LAST_NAME,
+            PHONE: foundUser[0].PHONE,
+            BIRTH_DATE: foundUser[0].BIRTH_DATE,
+            ADDRESS: foundUser[0].ADDRESS,
+            ADDITIONAL_ADDRESS: foundUser[0].ADDITIONAL_ADDRESS,
+            POSTAL_CODE: foundUser[0].POSTAL_CODE,
+            CITY: foundUser[0].CITY,
+            IMAGE_LINK: foundUser[0].IMAGE_LINK,
+            METROPOLISES_ID: foundUser[0].METROPOLISES_ID,
+            ACCEPTED: foundUser[0].ACCEPTED
+        },
         authenticationToken: token
-	});
+    });
 };
 
 /**
@@ -198,7 +212,7 @@ module.exports = {
     deleteRenter,
     getRenterList,
     getRenterByEquipment,
-	createRenter,
-	getListMailRenter, 
-	loginRenter
+    createRenter,
+    getListMailRenter,
+    loginRenter
 }
